99155b41a2fbfd7c3e64a9bbf4b97213a1adfe35
vanessacezarn <vanessacezar3449@gmail.com>
2025-02-25 10:01:15 -0300

    Create exercicio02.java

---
+++ Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/exercicio02.java
@@ -0,0 +1,17 @@
+package exercicio02;
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		System.out.println("Numeros pares de 2 a 20");
+		for(i=2;i<21;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
