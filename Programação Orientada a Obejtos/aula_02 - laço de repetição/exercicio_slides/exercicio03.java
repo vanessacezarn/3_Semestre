46cf1435c19ad22783655516a7b9164b73cbe2c8
vanessacezarn <vanessacezar3449@gmail.com>
2025-02-25 10:07:57 -0300

    Create exercicio03.java

---
+++ Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/exercicio03.java
@@ -0,0 +1,18 @@
+package exercicio03;
+
+public class Exercicio03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,a=1;
+		System.out.println("TABUADA DO 7");
+		
+		for(i=1;i<11;i++) {
+			a=7*i;
+			System.out.println("7 x "+i+" = "+a);
+			a=1;
+			
+		}
+	}
+
+}
