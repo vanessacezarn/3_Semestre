978a762a1da816df31982b31bd0e2a4663b676b2
vanessacezarn <vanessacezar3449@gmail.com>
2025-02-25 09:59:56 -0300

    Create vetor.java

---
+++ Programação Orientada a Obejtos/aula_02 - laço de repetição/vetor.java
@@ -0,0 +1,22 @@
+package vetor;
+
+public class VETOR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] vetor = new int [5];
+		
+		vetor[0] =5;
+		vetor[1] =8;
+		vetor[2] =6;
+		vetor[3] =4;
+		vetor[4] =7;
+
+		for(int i=0;i<5;i++) {
+			System.out.print("ELEMENTO "+i+": ");
+			System.out.println(vetor[i]);
+		}
+
+	}
+
+}
