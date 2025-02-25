2fdd3ffb7f55f59de7aad406445e92250294f4f2
vanessacezarn <vanessacezar3449@gmail.com>
2025-02-25 10:17:06 -0300

    Create exercicio05.java

---
+++ Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/exercicio05.java
@@ -0,0 +1,26 @@
+package exercicio05;
+import java.util.Scanner;
+
+
+public class Exercicio05 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		int n,i,res=1;
+		
+		System.out.println("Digite um numero para calcular o fatorial:");
+		n=teclado.nextInt();
+		
+		for(i=n;i>=1;i--) {
+		res *= i;
+			
+		}
+		System.out.println(res);
+	
+		teclado.close();
+	}
+	
+
+}
+
