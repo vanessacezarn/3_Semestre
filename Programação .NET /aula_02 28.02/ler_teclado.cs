1fe40736ee558321928bd63bb138b0dac8b35166
vanessacezarn <vanessacezar3449@gmail.com>
2025-02-28 12:52:36 -0300

    Create ler_teclado.cs

---
+++ Programação .NET /aula_02 28.02/ler_teclado.cs
@@ -0,0 +1,21 @@
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Digite um valor: ");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Numero digitado = " +num); //line = \n
+
+            double num2;
+            Console.Write("Digite um valor colm virgula: ");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Numero digitado = " + num2);
+
+
+
+        }
+    }
+}
