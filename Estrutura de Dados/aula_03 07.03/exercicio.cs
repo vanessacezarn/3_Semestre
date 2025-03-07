6ae74903ee49284fe6b817c5e88992cfa8138270
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-07 09:56:04 -0300

    Create exercicio.cs

---
+++ Estrutura de Dados/aula_03 07.03/exercicio.cs
@@ -0,0 +1,3 @@
+//Exercicio - 1) c# exercicio dos nomes
+//             2) java e c# - gerar valores(n valores) de glicemia aleatórios entre 50 e 240.
+//                A)aplicar a média B) aplicar a mediana - ordenar - capturar o valor do meio C) aplicar a moda
