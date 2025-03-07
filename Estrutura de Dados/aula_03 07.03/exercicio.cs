b384cf19bd5dd0827f9189afd05d0404dcbed4a3
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-07 10:06:02 -0300

    Create exercicio.cs

---
+++ Estrutura de Dados/aula_03 07.03/exercicio.cs
@@ -0,0 +1,3 @@
+//Lista 1 - com n números aleatórios tamanho n - números de 0 a 100 e sem repetições
+//Lista 2 - com n números aleatórios tamanho n - números de 0 a 100 e sem repetições
+//Lista 3 - números comuns nas 2 listas
