4f6ad240ff5c0dc42914f34dc8e71fd5ed4ec390
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-07 11:57:23 -0300

    Create operaçoes_basicas.cs

---
+++ Programação .NET /aula_03 07.03/operaçoes_basicas.cs
@@ -0,0 +1,44 @@
+namespace aula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            double num1;
+            Console.Write("Digite um valor com virgula: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Numero digitado = " + num1);
+
+            double num2;
+            Console.Write("Digite um valor com virgula: ");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Numero digitado = " + num2);
+
+            double soma;
+            soma = num1 + num2;
+            Console.WriteLine("SOMA dos valores digitado: "+num1+" + "+num2 +" = "+ soma);
+
+            double diferenca;
+            diferenca = num1 - num2;
+            Console.WriteLine("SUBTRAÇÃO dos valores digitado = " + num1 + " - " + num2 + " = " + diferenca);
+
+            double multi;
+            multi = num1 * num2;
+            Console.WriteLine("MULTIPLICAÇÃO dos valores digitado = " + num1 + " * " + num2 + " = " + multi);
+
+            double divisao;
+            divisao = num1 / num2;
+            Console.WriteLine("DIVISÃO dos valores digitado = " + num1 + " / " + num2 + " = " + divisao);
+
+
+
+
+
+
+
+            //fim
+        }
+    }
+}
