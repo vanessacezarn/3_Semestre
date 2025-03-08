7b6f81c4af88ae7e2fc71d97234cffbb1e9ce0dd
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-08 16:33:35 -0300

    Create exercicio06.java

---
+++ Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/vetores e matrizes/exercicio06.java
@@ -0,0 +1,2 @@
+//6 - Escreva um programa Java que use um vetor para ler os nomes de 5 cidades e outro vetor para armazenar as suas respectivas populações e 
+//depois imprima na tela o nome da cidade mais populosa.
