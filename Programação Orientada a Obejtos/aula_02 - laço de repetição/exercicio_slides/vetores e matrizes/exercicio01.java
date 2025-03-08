a7ee08784eb26589150550dc0548967fdf150587
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-08 16:58:11 -0300

    Update exercicio01.java

--- Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/vetores e matrizes/exercicio01.java
+++ Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/vetores e matrizes/exercicio01.java
@@ -1,1 +1,31 @@
 //1 - Escreva um programa Java que use um vetor para ler 5 nomes de pessoas e depois os imprima na tela.
+
+package vetor_matriz_ex01;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner (System.in);
+		
+		String [] nomes = new String [5];
+		int i;
+		
+		for (i=0;i<5;i++) {
+			System.out.print("Digite um nome:");
+			nomes[i] = teclado.next();	
+		}
+		
+		System.out.println("\tNOMES DIGITADOS");
+		for(i=0;i<5;i++) {
+			System.out.println(nomes[i]);
+		}
+		
+		
+		
+		teclado.close();
+	}
+
+}
