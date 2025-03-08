142c44e190122e9a3f27e6151879080142b70e54
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-08 16:34:01 -0300

    Create exercicio07.java

---
+++ Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/vetores e matrizes/exercicio07.java
@@ -0,0 +1,2 @@
+// 7 - Escreva um programa Java que use uma matriz para armazenar as notas de 3 alunos em 4 disciplinas e depois imprima na tela a nota mais alta e a 
+//nota mais baixa em cada disciplina
