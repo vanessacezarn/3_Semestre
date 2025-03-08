1c468e42fe0a2445a01e92037cfd6e38d1dab78e
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-08 16:21:41 -0300

    Update exercicio06.java

--- Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/exercicio06.java
+++ Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/exercicio06.java
@@ -1,20 +1,24 @@
 //6- Escreva um programa Java que use um laço para ler uma sequência de números inteiros do usuário até que o número 0 seja lido.
 
-package exercicio06;
+package exercicio06_repeticao;
+
 import java.util.Scanner;
 
-public class Exercicio06 {
+public class Ex6 {
 
 	public static void main(String[] args) {
-		Scanner teclado = new Scanner(System.in);
+		// TODO Auto-generated method stub
 		int n;
-		
-		
-		do{
-			System.out.print("digite um número inteiro: ");
+		Scanner teclado = new Scanner(System.in);
+		do {
+			System.out.print("Digite um número: ");
 			n=teclado.nextInt();
+			System.out.println("digite 0 para SAIR");
 		}while(n!=0);
 		
+		if(n==0) {
+			System.out.println("FIM DO PROGRAMA ");
+		}
 		
 		teclado.close();
 	}
