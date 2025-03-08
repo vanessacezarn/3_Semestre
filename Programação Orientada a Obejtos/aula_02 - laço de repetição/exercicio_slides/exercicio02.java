6b0befc85798e9b12b2c2da740e8bce8f2e9b891
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-08 16:12:46 -0300

    Update exercicio02.java

--- Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/exercicio02.java
+++ Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/exercicio02.java
@@ -1,19 +1,20 @@
 //2 - Escreva um programa Java que use um laço para imprimir os números pares de 2 a 20.
 
-package exercicio02;
+package exercicio02_repeticao;
 
-public class Exercicio02 {
+public class Ex2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i;
-		System.out.println("Numeros pares de 2 a 20");
+		System.out.println("Números pares de 2 a 20:");
 		for(i=2;i<21;i++) {
-			if(i%2==0) {
+			if(i % 2 == 0) {
 				System.out.println(i);
 			}
 		}
-		
+			
+
 	}
 
 }
