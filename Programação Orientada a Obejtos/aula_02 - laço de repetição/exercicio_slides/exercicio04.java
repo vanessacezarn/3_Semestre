dfaa5f9900acc82af5dc2f40cd110a2a54b11742
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-08 16:13:22 -0300

    Update exercicio04.java

--- Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/exercicio04.java
+++ Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/exercicio04.java
@@ -1,17 +1,15 @@
  //4 - Escreva um programa Java que use um laço para somar os números de 1 a 100.
-package exercicio04;
+package exercicio04_repeticao;
 
-public class Exercicio04 {
+public class Ex4 {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		int i,soma=0;
-		
-		for(i=1;i<101;i++) {
+		for(i=0;i<101;i++) {
 			soma=soma+i;
-		
 		}
-		System.out.print("SOMA DOS NÚMEROS DE 1 A 100 = ");
-		System.out.println(soma);
+		System.out.println("Soma dos números de 1 a 100 = "+soma);
 	}
 
 }
