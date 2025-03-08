a925f9539cdc5f049245eb4888d7695c1034183a
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-08 16:12:29 -0300

    Update exercicio01.java

--- Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/exercicio01.java
+++ Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/exercicio01.java
@@ -1,12 +1,13 @@
 //1 - Escreva um programa Java que use um laço imprimir os números de 1 a 10.
-package aula02;
+package exercicio01_repeticao;
 
-public class A1 {
+public class Ex1 {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		int i;
-		
-		for(i=1;i<11;i++) {
+		System.out.println("números de 1 a 10");
+		for (i=1;i<11;i++) {
 			System.out.println(i);
 		}
 
