8c1f505fc1b82eca2abe96aaee54eec0a27193be
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-09 22:06:52 -0300

    Update exercicio02.java

--- Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/vetores e matrizes/exercicio02.java
+++ Programação Orientada a Obejtos/aula_02 - laço de repetição/exercicio_slides/vetores e matrizes/exercicio02.java
@@ -1,1 +1,25 @@
 // 2 - Escreva um programa Java que use uma matriz para ler os valores de uma tabela de multiplicação (tabuada) de 10x10 e depois a imprima na tela.
+package vetor_matriz_ex02;
+
+public class Ex2 {
+	
+    public static void main(String[] args) {
+    	
+        int[][] tabuada = new int[10][10];
+        int i,j;
+        
+        for (i = 0; i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+                tabuada[i][j] = (i+1) * (j+1);
+            }
+        }
+        for (i = 0;i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+                System.out.printf("%4d", tabuada[i][j]); //PARA FORMATAR A TABELA
+
+            }
+            System.out.println();
+        }
+        
+    }
+}
