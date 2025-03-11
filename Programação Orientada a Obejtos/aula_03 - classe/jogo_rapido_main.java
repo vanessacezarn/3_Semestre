8d780c8c36e93da33c0dc91ea95a4f1e19a10685
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-11 09:57:46 -0300

    Update jogo_rapido_main.java

--- Programação Orientada a Obejtos/aula_03 - classe/jogo_rapido_main.java
+++ Programação Orientada a Obejtos/aula_03 - classe/jogo_rapido_main.java
@@ -13,17 +13,18 @@ public class Disciplina {
 		
 		p.nome = "Ricardo da Silva";
 		l.local = "Sala 311";
-		
-		/*
-		System.out.println("nome: ");
-		p.nome= teclado.nextLine();
-		System.out.println("sala: ");
-		l.local = teclado.nextLine();
-		*/
-		
 		System.out.println("o nome do professor é "+p.nome);
 		System.out.println("o local da aula é "+l.local);
 		
+		
+		Professor p2 = new Professor();
+		Laboratorio l2 = new Laboratorio ();
+		System.out.println("nome do professor: ");
+		p2.nome= teclado.nextLine();
+		System.out.println("sala de aula: ");
+		l2.local = teclado.nextLine();
+		System.out.println("o nome do professor é "+p2.nome);
+		System.out.println("o local da aula é "+l2.local);
 		
 		teclado.close();
 	}
