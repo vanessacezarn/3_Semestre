8f17974b6748cfb9aa0470108ad1dc2e3121d9a4
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-13 22:18:34 -0300

    Update exercicio_glicemia.cs

--- Estrutura de Dados/aula_03 07.03/exercicio_glicemia.cs
+++ Estrutura de Dados/aula_03 07.03/exercicio_glicemia.cs
@@ -2,10 +2,6 @@
 //             2) java e c# - gerar valores(n valores) de glicemia aleatórios entre 50 e 240.
 //                A)aplicar a média B) aplicar a mediana - ordenar - capturar o valor do meio C) aplicar a moda
 
-//Exercicio - 1) c# exercicio dos nomes
-//             2) java e c# - gerar valores(n valores) de glicemia aleatórios entre 50 e 240.
-//                A)aplicar a média B) aplicar a mediana - ordenar - capturar o valor do meio C) aplicar a moda
-
 using System.Net.Mime;
 
 Console.WriteLine("GLICEMIA");
