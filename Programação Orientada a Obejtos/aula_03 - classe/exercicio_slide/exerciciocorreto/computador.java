115042518a3a9d06ffc32dc19fcdc865da303f44
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-18 07:59:52 -0300

    Update computador.java

--- Programação Orientada a Obejtos/aula_03 - classe/exercicio_slide/exerciciocorreto/computador.java
+++ Programação Orientada a Obejtos/aula_03 - classe/exercicio_slide/exerciciocorreto/computador.java
@@ -1,3 +1,7 @@
+//2 - Crie uma classe chama Computador que possua 4 atributos: marca, modelo, tipo (notebook,netbook, tablet, etc) e preço. Essa classe Computador também deverá ser executável, entretanto crie uma outra classe
+//executável que irá instanciar a classe Computador, criando 2 objetos. O primeiro objeto deveráser criado através da solicitação dos valores ao usuário por linha de execução. Já, os valores dosegundo objeto, 
+//deverão ser definidos no código-fonte da classe que o criou. Exiba na tela os resultados.
+
 package exercicio2;
 
 import java.util.Scanner;
