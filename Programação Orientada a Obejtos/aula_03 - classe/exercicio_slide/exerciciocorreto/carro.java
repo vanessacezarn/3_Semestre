1dff58fff53eb3ebfa4d946fc89261304f16b4bb
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-18 07:58:31 -0300

    Update carro.java

--- Programação Orientada a Obejtos/aula_03 - classe/exercicio_slide/exerciciocorreto/carro.java
+++ Programação Orientada a Obejtos/aula_03 - classe/exercicio_slide/exerciciocorreto/carro.java
@@ -1,3 +1,6 @@
+//1 - Crie uma classe chamada Carro que possua 3 atributos: marca, modelo e anoFabricacao.Além disso, crie uma classe executável para criar 2 objetos do tipo Carro e exibir na tela os
+//valores dos atributos criados. Esses valores devem ser solicitados ao usuário, por linha de execução.
+
 package exercicio1;
 
 public class Carro {
