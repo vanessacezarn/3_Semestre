cf96b13437b1f623ff1f4fa96da4242a8f489c19
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-23 20:02:23 -0300

    Create exercicio03_executavel.java

---
+++ Programação Orientada a Obejtos/aula_04 - construtores/exercicios_slides/exercicio03_executavel.java
@@ -0,0 +1,46 @@
+//3) Crie uma classe chamada Pessoa. Nesta classe deve conter os atributos: Nome, email, data de nascimento, Endereço e o atributo Admin do 
+//tipo booleano. Crie um método que retorne o Email da pessoa. Crie um método chamado promoverAdmin que ao ser chamado altera o atributo Admin.
+//Faça a leitura de duas pessoas na main e mantenha uma como admin e outra não.
+
+package exercicio03;
+
+import java.util.Scanner;
+
+public class Ex3 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+		
+		Pessoa p = new Pessoa();
+		System.out.println("PESSOA 1");
+		System.out.print("Digite o nome:");
+		p.nome=teclado.nextLine();
+		System.out.print("Digite o email:");
+		p.email=teclado.nextLine();
+		System.out.print("Digite a data de nascimeto:");
+		p.data_nascimento=teclado.nextLine();
+		System.out.print("Digite o endereço:");
+		p.endereco=teclado.nextLine();
+		String retorno;
+		retorno = p.Email();
+		System.out.println("o email da pessoa 1 é "+retorno);
+		
+		Pessoa p2 = new Pessoa();
+		System.out.println("PESSOA 2");
+		System.out.print("Digite o nome:");
+		p2.nome=teclado.nextLine();
+		System.out.print("Digite o email:");
+		p2.email=teclado.nextLine();
+		System.out.print("Digite a data de nascimeto:");
+		p2.data_nascimento=teclado.nextLine();
+		System.out.print("Digite o endereço:");
+		p2.endereco=teclado.nextLine();
+		p2.Email();
+		retorno = p2.Email();
+		System.out.println("o email da pessoa 1 é "+retorno);
+		p2.promoverAdmin();
+		
+		
+		teclado.close();
+	}
+
+}
