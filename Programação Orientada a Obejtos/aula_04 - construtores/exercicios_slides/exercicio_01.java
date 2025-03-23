82ec2ed0dd160950a82f42ecc3cf98cbf64d70d3
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-23 17:18:03 -0300

    Delete Programação Orientada a Obejtos/aula_04 - construtores/exercicios_slides/exercicio_01.java

--- Programação Orientada a Obejtos/aula_04 - construtores/exercicios_slides/exercicio_01.java
+++
@@ -1,2 +0,0 @@
-//1) Crie uma classe Disciplina que contenha os atributos (nome, carga horária e nome do professor) e os métodos de atribuir e recuperar esses valores. Mostre na tela:
-//a) O conteúdo original dos atributos   b) A opção para o usuário inserir os valores    c) A exibição do novo conteúdo dos atributos
