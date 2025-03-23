de83e0e0753568e40e0ed7e78fdedcd5b1e60f5f
vanessacezarn <vanessacezar3449@gmail.com>
2025-03-23 09:18:49 -0300

    Update exercicio.java

--- Estrutura de Dados/aula_05 21.03/exercicio.java
+++ Estrutura de Dados/aula_05 21.03/exercicio.java
@@ -1,3 +1,10 @@
+//Listas: dicionário ou objeto
+//Fazer um programa em Java, C# ou C++ que receba nomes completos de pessoas (validar se nome foi digitado completo). A partir disso, gerar email da 
+//pessoa combinando o primeiro nome com o último nome, separados por '.', mais '@ufn.edu.br'. Por exemplo, João Pedro Garcia -> joao.garcia@ufn.edu.br.
+//Uma vez que o email foi gerado, adicionar o par (nome completo e email) em uma lista. Essa lista pode ser composta por dicionários 
+//(email será a chave e nome completo o valor) ou por objetos (da classe Pessoa, com atributos nome e email). Contudo, não pode haver emails repetidos. 
+//Ao final, exibir a lista com os dados (nomes e emails).
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
