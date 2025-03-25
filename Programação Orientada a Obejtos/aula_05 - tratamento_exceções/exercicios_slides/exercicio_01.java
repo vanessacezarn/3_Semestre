9253008b5a2bbc9ffeb043e693e671d7cf2104c9
Vanessa Cezar <vanessacezar3449@gmail.com>
2025-03-25 10:19:00 -0300

    Update exercicio_01.java

--- Programação Orientada a Obejtos/aula_05 - tratamento_exceções/exercicios_slides/exercicio_01.java
+++ Programação Orientada a Obejtos/aula_05 - tratamento_exceções/exercicios_slides/exercicio_01.java
@@ -1,1 +1,33 @@
-.
+//1 - Escreva um programa que solicita ao usuário para digitar um número inteiro e exiba a raiz quadrada desse número. Certifique-se de que 
+//o programa trate adequadamente as exceções se o usuário digitar um valor inválido, por exemplo, ler um char ou string.
+
+package exercicios_slidses;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int n;
+		double raiz;
+		
+		System.out.print("Digite um número inteiro: ");
+		//n = teclado.nextInt();
+		try {
+		n = teclado.nextInt();
+		raiz= Math.sqrt(n);
+		System.out.println("raiz ="+raiz);
+		}catch(Exception e) {
+			System.out.println("exceção:"+e.toString());
+			System.out.println("programa continua");
+			
+		}
+	
+		
+		teclado.close();
+	}
+
+}
+//ERRO : InputMismatchException
