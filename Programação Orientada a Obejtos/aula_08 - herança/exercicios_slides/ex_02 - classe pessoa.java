ca7a4892510f7eec63bb1377e4d549a41e347270
Vanessa Cezar <vanessacezar3449@gmail.com>
2025-04-29 08:25:43 -0300

    Create ex_02 - classe pessoa.java

---
+++ Programação Orientada a Obejtos/aula_08 - herança/exercicios_slides/ex_02 - classe pessoa.java
@@ -0,0 +1,17 @@
+package exercicio2;
+
+public class Pessoa {
+	
+	protected String nome;
+
+	public Pessoa(String nome) {
+		super();
+		this.nome = nome;
+	}
+	
+	public void trabalhar() {
+		System.out.println("A pessoa está trabalhando");
+	}
+
+
+}
