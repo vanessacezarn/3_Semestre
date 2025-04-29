407ea07e45aaca3ad44b2ab5d0a1a2af4b8d42c5
Vanessa Cezar <vanessacezar3449@gmail.com>
2025-04-29 08:22:42 -0300

    Create ex_02-classe principal.java

---
+++ Programação Orientada a Obejtos/aula_08 - herança/exercicios_slides/ex_02-classe principal.java
@@ -0,0 +1,14 @@
+
+package exercicio2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Funcionario f = new Funcionario("Vanessa");
+		System.out.println("nome do funcionário: "+f.nome);
+		f.trabalhar();
+		
+	}
+
+}
