e82c93d48866fe795eff066fe77ce515f099b01a
Vanessa Cezar <vanessacezar3449@gmail.com>
2025-05-04 20:02:55 -0300

    Add files via upload

---
+++ Programação .NET /aula_10 02.05 - encapsulamento/exercicio_slides/exercicio_05_Estudante/Estudante.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio05
+{
+    class Estudante
+    {
+        private String _nome;
+        private double _notaFinal;
+
+        public Estudante(string nome, double notaFinal)
+        {
+            Nome = nome;
+            _notaFinal = notaFinal;
+        }
+        public String Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        public String GetNotaFinal()
+        {
+            if (_notaFinal >= 70)
+            {
+                return "APROVADO";
+            }
+            else
+            {
+                return "REPROVADO";
+            }
+        }
+
+    }
+}
---
+++ Programação .NET /aula_10 02.05 - encapsulamento/exercicio_slides/exercicio_05_Estudante/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Defina uma classe Estudante que contém as propriedades Nome e uma propriedade privada notaFinal.  
+A notaFinal deve ser acessível apenas através de um método GetNotaFinal(), que deve retornar "Aprovado" 
+se a nota for maior ou igual a 70 e "Reprovado" se for menor*/
+namespace exercicio05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante e = new Estudante("Vanessa", 60);
+            Console.WriteLine("Estudante 1");
+            Console.WriteLine("Nome: "+e.Nome);
+            Console.Write("Situação: "+ e.GetNotaFinal());
+
+            Estudante e2 = new Estudante("Amanda", 89);
+            Console.WriteLine("\n\nEstudante 2");
+            Console.WriteLine("Nome: " + e2.Nome);
+            Console.Write("Situação: " + e2.GetNotaFinal());
+
+        }
+    }
+}
