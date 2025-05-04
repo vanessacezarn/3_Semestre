74191625f82e5524277e598392f6ea26bd935e19
Vanessa Cezar <vanessacezar3449@gmail.com>
2025-05-04 20:00:24 -0300

    Add files via upload

---
+++ Programação .NET /aula_10 02.05 - encapsulamento/exercicio_slides/exercicio_03_Pessoa/Pessoa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*3) Crie uma classe Pessoa com as propriedades Nome e Idade. Adicione validação nas propriedades para que:  
+a) Nome não aceite valores nulos ou strings vazias.  
+b) Idade não aceite valores menores que 0 ou maiores que 120.*/
+namespace exercicio03
+{
+    class Pessoa
+    {
+        private String _nome;
+        private int _idade;
+
+        public String Nome
+        {
+            get { return _nome; }
+            set {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("nome não pode ser nulo ou vazio");
+                }
+                else
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public int Idade
+        {
+            get { return _idade; }
+            set {
+                if (value < 0 || value > 120) {
+                    throw new ArgumentException("idade não pode ser menor que 0 ou maior que 120");
+                } else {
+                    _idade = value;
+                }
+            }
+
+        }
+    }
+}
---
+++ Programação .NET /aula_10 02.05 - encapsulamento/exercicio_slides/exercicio_03_Pessoa/Program.cs
@@ -0,0 +1,25 @@
+﻿/*3) Crie uma classe Pessoa com as propriedades Nome e Idade. Adicione validação nas propriedades para que:  
+a) Nome não aceite valores nulos ou strings vazias.  
+b) Idade não aceite valores menores que 0 ou maiores que 120.*/
+
+namespace exercicio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+
+            Console.Write("digite um nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("digite uma idade: ");
+            p.Idade = int.Parse(Console.ReadLine());          
+            Console.WriteLine("\nnome: " + p.Nome);
+            Console.WriteLine("idade: " + p.Idade);
+
+
+
+
+        }
+    }
+}
