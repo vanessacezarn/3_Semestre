bdf6e8cc3b781fd2c56be7c58c23407f0bc95c01
Vanessa Cezar <vanessacezar3449@gmail.com>
2025-05-04 19:57:52 -0300

    Add files via upload

---
+++ Programação .NET /aula_10 02.05 - encapsulamento/exercicio_slides/ContaBancaria.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*2) Crie uma classe ContaBancaria com os seguintes atributos: NumeroConta, Saldo e Titular.
+Criar propriedades para acesso (get e set) aos dados.
+Analise as necessidades de encapsulamento.
+Crie os métodos construtor, public bool Depositar(double valor), public bool Sacar(double valor) e public void ExibirSaldo()*/
+namespace exercicio02
+{
+    class ContaBancaria
+    {
+        private String _numeroConta;
+        private String _titular;
+        private double _saldo;
+
+        //NUMERO CONTASÓ PODE SER OBTIDOS, POIS NÃO SE PODE MUDAR O Nº DA CONTA DEPOIS DE ABERTA UMA CONTA
+        public String NumeroConta
+        {
+            get { return _numeroConta; }
+        }
+        public String Titular
+        {
+            get { return _titular; }
+            set { _titular = value; }
+        }
+
+        public double Saldo
+        {
+            get { return _saldo; }
+            set { _saldo = value; }
+        }
+
+        public ContaBancaria(String numeroConta, string nome, double saldo)
+        {
+            _numeroConta = numeroConta;
+            _titular = nome;
+            Saldo = saldo;
+        }
+        public bool Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo = Saldo + valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if(_saldo < valor)
+            {
+                return false;
+            }
+            else {
+                Saldo = Saldo - valor;
+                return true;
+            }
+        }
+        public void ExibirSaldo()
+        {
+            Console.WriteLine("saldo = "+Saldo);
+        }
+    }
+}
---
+++ Programação .NET /aula_10 02.05 - encapsulamento/exercicio_slides/Program.cs
@@ -0,0 +1,76 @@
+﻿/*2) Crie uma classe ContaBancaria com os seguintes atributos: NumeroConta, Saldo e Titular.
+Criar propriedades para acesso (get e set) aos dados.
+Analise as necessidades de encapsulamento.
+Crie os métodos construtor, public bool Depositar(double valor), public bool Sacar(double valor) e public void ExibirSaldo()*/
+namespace exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria cb = new ContaBancaria("1574-5", "Vanessa Cezar", 1500);
+            Console.WriteLine("Informações sobre a conta bancaria ");
+            Console.WriteLine("Nome do titular : "+cb.Titular);
+            Console.WriteLine("Número da conta : "+cb.NumeroConta);
+            cb.ExibirSaldo();
+            int op=0;
+            do
+            {
+                Console.WriteLine("\n\tMENU");
+                Console.WriteLine("1 - Sacar");
+                Console.WriteLine("2 - Depositar");
+                Console.WriteLine("3 - Saldo");
+                Console.WriteLine("4 - Informações da conta");
+                Console.WriteLine("5 - Sair");
+                Console.Write("digite uma opção: ");
+                op = int.Parse(Console.ReadLine());
+                if (op == 1)
+                {
+                    Console.Write("\nDigite o valor que deseja sacar:");
+                    double saque = double.Parse(Console.ReadLine());
+                    Boolean retorno = cb.Sacar(saque);
+                    if (retorno == true)
+                    {
+                        Console.WriteLine("Saque realizado com sucesso !!! ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERRO : saldo insuficiente");
+                    }
+                }
+                else if (op == 2)
+                {
+                    Console.Write("\nDigite o valor que deseja depositar:");
+                    double deposito = double.Parse(Console.ReadLine());
+                    Boolean retorno = cb.Depositar(deposito);
+                    if (retorno == true)
+                    {
+                        Console.WriteLine("Depósito realizado com sucesso");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERRO : valor digitado é inválido");
+                    }
+                }
+                else if (op == 3)
+                {
+                    cb.ExibirSaldo();
+                }else if (op == 4) {
+                    Console.WriteLine("\nInformações da conta");
+                    cb.Titular = "Vanessa Cezar do Nascimento"; //PARA MOSTRAR QUE É POSSIVEL ALTERAR O NOME DO TITULAR
+                    Console.WriteLine("Nome do titular : " + cb.Titular);
+                    Console.WriteLine("Número da conta : " + cb.NumeroConta);
+
+                }
+                else if(op == 5)
+                {
+                    Console.WriteLine("\nFIM DO PROGRAMA");
+                }
+                else
+                {
+                    Console.WriteLine("opção inválida!! tente novamente");
+                }
+            } while (op != 5);
+        }
+    }
+}
