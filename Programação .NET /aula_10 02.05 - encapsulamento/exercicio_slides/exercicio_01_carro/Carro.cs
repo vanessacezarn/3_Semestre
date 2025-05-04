7e52306a02e7d8b442c4f5352209c7e8e84b4d77
Vanessa Cezar <vanessacezar3449@gmail.com>
2025-05-04 19:57:00 -0300

    Add files via upload

---
+++ Programação .NET /aula_10 02.05 - encapsulamento/exercicio_slides/exercicio_01_carro/Carro.cs
@@ -0,0 +1,51 @@
+﻿//Crie uma classe Carro com três propriedades: Marca, Modelo e Ano.
+//Use o encapsulamento adequado para garantir que:
+//a) A Marca e o Modelo possam ser lidos e escritos externamente.
+//b) O Ano só possa ser definido no construtor e acessado (mas não modificado) depois disso.
+namespace exercicio01
+{
+    class Carro
+    {
+        private String _modelo;
+        private String _marca;
+        private int _Ano;
+
+        public Carro(String _modelo, String _marca, int _Ano)
+        {
+            this._modelo = _modelo;
+            this._marca = _marca;
+            this._Ano = _Ano;
+        }
+        public String Modelo
+        {
+            get{
+                return _modelo;
+            }
+            set
+            {
+                _modelo = value;
+            }
+        }
+
+        public String Marca
+        {
+            get
+            {
+                return _marca;
+            }
+            set
+            {
+                _marca = value;
+            }
+        }
+
+        public int Ano
+        {
+            get
+            {
+                return _Ano;
+            }
+        }
+
+    }
+}
---
+++ Programação .NET /aula_10 02.05 - encapsulamento/exercicio_slides/exercicio_01_carro/Program.cs
@@ -0,0 +1,25 @@
+﻿//Crie uma classe Carro com três propriedades: Marca, Modelo e Ano.
+//Use o encapsulamento adequado para garantir que:
+//a) A Marca e o Modelo possam ser lidose escritos externamente.
+//b) O Ano só possa ser definido no construtor e acessado (mas não modificado) depois disso.
+namespace exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c = new Carro("Ka", "Ford", 2025);
+            Console.WriteLine("modelo: "+c.Modelo);
+            Console.WriteLine("marca: "+c.Marca);
+            Console.WriteLine("ano: "+c.Ano);
+
+            Console.WriteLine("\ntestando modificações");
+            c.Modelo = "gol";
+            c.Marca = "VW";
+            Console.WriteLine("modelo: " + c.Modelo);
+            Console.WriteLine("marca: " + c.Marca);
+
+
+        }
+    }
+}
