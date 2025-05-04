b59d819748e02c5366af90d4e3356387a40c1fdd
Vanessa Cezar <vanessacezar3449@gmail.com>
2025-05-04 20:04:24 -0300

    Add files via upload

---
+++ Programação .NET /aula_10 02.05 - encapsulamento/exercicio_slides/exercicio_04_retangulo/Program.cs
@@ -0,0 +1,18 @@
+﻿/*4) Crie uma classe Retangulo com propriedades para Largura e Altura, e uma propriedade calculada Area. 
+ * A área deve ser calculada como Largura * Altura. */
+namespace exercicio04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.Write("digite a largura do retângulo: ");
+            r.Largura = double.Parse(Console.ReadLine());
+            Console.Write("digite a altura do retângulo: ");
+            r.Altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("A área do retângulo digitada é de "+ r.Area());
+        }
+    }
+}
---
+++ Programação .NET /aula_10 02.05 - encapsulamento/exercicio_slides/exercicio_04_retangulo/Retangulo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio04
+{
+    class Retangulo
+    {
+        private double _largura;
+        private double _altura;
+
+        public double Largura { 
+            get { return _largura; } 
+            set {
+                if (value <= 0) {
+                    throw new ArgumentException("a largura tem que ser maior que 0 ");
+                } else {
+                    _largura = value;
+                }
+            }
+        }
+        public double Altura
+        {
+            get { return _altura; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("a altura tem que ser maior que 0 ");
+                }
+                else
+                {
+                    _altura = value;
+                }
+            }
+        }
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+
+    }
+}
