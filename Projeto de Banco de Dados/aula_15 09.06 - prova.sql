db76563702c52f23ecbf6c7482ba06f28887b24a
Vanessa Cezar <vanessacezar3449@gmail.com>
2025-06-09 09:38:00 -0300

    Update and rename aula_15 09.06 - prova to aula_15 09.06 - prova.sql

---
+++ Projeto de Banco de Dados/aula_15 09.06 - prova.sql
@@ -0,0 +1,38 @@
+
+-- 1)selecione autores (nomes) que escrevem livros do genero ficcao
+
+select autor.nome
+from autor, livro, genero,livro_autor
+where (genero.descricao ="Ficção") and  genero.idGenero = livro.idGenero and livro_autor.idLivro= livro.idLivro and livro_autor.idAutor=autor.idAutor;
+
+-- 2) nome das editoras que tem autores escrevendo infantil
+select editora.nome
+from editora, genero, livro
+where (genero.descricao ="Infantil") and genero.idGenero = livro.idGenero and livro.idEditora = editora.idEditora;
+
+-- 3) mostre os livros, com seus generos e editora que o autor francisco escreveu 
+select livro.titulo, genero.descricao, editora.nome
+from livro, genero,editora,livro_autor, autor
+where (autor.nome = "Francisco") and autor.idAutor = livro_autor.idAutor and livro_autor.idLivro = livro.idLivro and genero.idGenero = livro.idGenero and editora.idEditora = livro.idEditora;
+
+-- 4) nome do autor que mais escreveu livro 
+
+
+select autor.nome
+from autor, livro_autor
+where autor.idAutor = livro_autor.idAutor
+group by autor.idAutor
+order by count(livro_autor.idLivro) desc
+limit 1;
+
+
+
+-- 5) genero mais publicado em todas as editora 
+
+
+select genero.descricao
+from genero, livro
+where genero.idGenero = livro.idGenero
+group by genero.idGenero
+order by count(livro.idLivro) desc
+limit 1;
